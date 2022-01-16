0daf15bb7e61182c09491a37ce72515a36cb0e9b
Luca <lfesta11@gmail.com>
2022-01-16 13:15:07 -0800

    refactored to use commands

--- main.py
+++ main.py
@@ -59,54 +59,55 @@ async def on_ready():
     print(f'Successfully logged in and booted...!')
 
 
-@bot.event
-async def on_message(message):
+# bot ignores own messages
+# if message.author == bot.user:
+#    return
+
+@bot.command()
+async def download(ctx, arg):
+    # command to convert song into downloadable .mp3
+    url = arg
+    raw_track_meta = from_url(url)
+    song_name = raw_track_meta["name"]
+    contributing_artists = ", ".join([artist["name"] for artist in raw_track_meta["artists"]])
+    file_name = f"{contributing_artists} - {song_name}.mp3"
+    await ctx.send("Downloading song...")
+    os.system(f"spotdl {url}")
+    os.rename(file_name, file_name.replace(" ", ""))
+    file_name = file_name.replace(" ", "")
+    await ctx.send('Here is your song:', file=discord.File(file_name))
+    os.remove(file_name)
+
+@bot.command()
+async def playlist(ctx, arg):
     global GUESSING, SONG_TITLE, GUESSING_AUTHOR
 
-    # bot ignores own messages
-    if message.author == bot.user:
-        return
+    url = arg
+    await ctx.send("Getting tracks...")
+    lst_of_name_link = playlist_songs_url(url)
+    await ctx.send("Picking a song...")
+    song_tuple = random.choice(lst_of_name_link)
+    raw_track_meta = from_url(song_tuple[1])
+    contributing_artists = ", ".join([artist["name"] for artist in raw_track_meta["artists"]])
+    song_name = raw_track_meta["name"]
+    file_name = f"{contributing_artists} - {song_name}.mp3"
+    await ctx.send("Loading song...")
+    os.system(f"spotdl {song_tuple[1]}")
+    os.rename(file_name, "full.mp3")
+    os.system('ffmpeg -i full.mp3 -ss 00:00:30 -t 00:00:05.0 -c copy song.mp3')
+    await ctx.send('Guess the song title', file=discord.File("song.mp3"))
+    os.remove("song.mp3")
+    os.remove("full.mp3")
+    GUESSING = True
+    SONG_TITLE = song_tuple[0]
+    GUESSING_AUTHOR = ctx.author
+    guess = await bot.wait_for('message')
 
-    # command to convert song into downloadable .mp3
-    if message.content.startswith('.download') and ' ' in message.content:
-        url = message.content.split(" ")[1]
-        raw_track_meta = from_url(url)
-        song_name = raw_track_meta["name"]
-        contributing_artists = ", ".join([artist["name"] for artist in raw_track_meta["artists"]])
-        file_name = f"{contributing_artists} - {song_name}.mp3"
-        await message.channel.send("Downloading song...")
-        os.system(f"spotdl {url}")
-        os.rename(file_name, file_name.replace(" ", ""))
-        file_name = file_name.replace(" ", "")
-        await message.channel.send('Here is your song:', file=discord.File(file_name))
-        os.remove(file_name)
-
-    elif message.content.startswith('.playlist'):
-        url = message.content.split(" ")[1]
-        await message.channel.send("Getting tracks...")
-        lst_of_name_link = playlist_songs_url(url)
-        await message.channel.send("Picking a song...")
-        song_tuple = random.choice(lst_of_name_link)
-        raw_track_meta = from_url(song_tuple[1])
-        contributing_artists = ", ".join([artist["name"] for artist in raw_track_meta["artists"]])
-        song_name = raw_track_meta["name"]
-        file_name = f"{contributing_artists} - {song_name}.mp3"
-        await message.channel.send("Loading song...")
-        os.system(f"spotdl {song_tuple[1]}")
-        os.rename(file_name, "full.mp3")
-        os.system('ffmpeg -i full.mp3 -ss 00:00:30 -t 00:00:05.0 -c copy song.mp3')
-        await message.channel.send('Guess the song title', file=discord.File("song.mp3"))
-        os.remove("song.mp3")
-        os.remove("full.mp3")
-        GUESSING = True
-        SONG_TITLE = song_tuple[0]
-        GUESSING_AUTHOR = message.author
-
-    elif GUESSING and message.author == GUESSING_AUTHOR:
-        if message.content == SONG_TITLE:
-            await message.channel.send("You guessed right!")
+    if GUESSING and guess.author == GUESSING_AUTHOR:
+        if guess.content == SONG_TITLE:
+            await ctx.send("You guessed right!")
         else:
-            await message.channel.send(f"Wrong answer! The song was {SONG_TITLE}")
+            await ctx.send(f"Wrong answer! The song was {SONG_TITLE}")
         GUESSING = False
 
 
