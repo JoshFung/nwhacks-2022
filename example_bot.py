f83dbf437086c75312c1b0d9a4a6b5e318b6b327
Josh Fung <joshuagyfung@gmail.com>
2022-01-16 04:46:57 -0800

    removed unnecessary comments in example_bot.py

--- example_bot.py
+++ example_bot.py
@@ -45,13 +45,13 @@ async def play(ctx, url : str):
 
     # run command
     os.system("spotdl " + url)
-    #
-    # snip_there = os.path.isfile("snip.mp3")
-    # try:
-    #     if snip_there:
-    #         os.remove("snip.mp3")
-    # except PermissionError:
-    #     await ctx.send("Wait for the current song to finish")
+
+    snip_there = os.path.isfile("snip.mp3")
+    try:
+        if snip_there:
+            os.remove("snip.mp3")
+    except PermissionError:
+        await ctx.send("Wait for the current song to finish")
 
     # rename song
     for file in os.listdir("./"):
