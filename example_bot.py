f20aac318aa37935a1f4978e0bbc5272e7fa9e7f
Josh Fung <joshuagyfung@gmail.com>
2022-01-15 22:03:46 -0800

    updated example_bot.py, added play(join) and disconnect functions

--- example_bot.py
+++ example_bot.py
@@ -1,21 +1,48 @@
 import discord
+from discord.ext import commands
 # import logging
 #
 # logging.basicConfig(level=logging.INFO)
 
-client = discord.Client()
+# client = discord.Client()
+bot = commands.Bot(command_prefix='>')
 
-@client.event
+
+@bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print(f'\n\nLogged in as: {bot.user.name} - {bot.user.id}\nVersion: {discord.__version__}\n')
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game(name="Testing!"))
+    print(f'Successfully logged in and booted...!')
 
-@client.event
-async def on_message(message):
-    # ignores its own messages
-    if message.author == client.user:
+
+@bot.command()
+async def test(ctx, *arg):
+    await ctx.send('{}'.format(' '.join(arg)))
+
+
+@bot.command()
+async def play(ctx, url : str):
+    # connects based on user's channel -- returns error if not in one
+    try:
+        vc = discord.utils.get(ctx.guild.voice_channels, name=ctx.author.voice.channel.name)
+    except AttributeError:
+        await ctx.send("You are not in a channel!")
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    if not voice.is_connect():
+        await vc.connect()
 
-client.run('OTMyMDE1NjkxMTY1NDkxMjIx.YeM1QA.80vYLWaj7ulCWK8n8dJWLepkWkU')
+
+@bot.command()
+async def disconnect(ctx):
+    if ctx.voice_client and ctx.author.voice.channel and ctx.author.voice.channel == \
+            ctx.voice_client.channel:
+        await ctx.voice_client.disconnect()
+        await ctx.send("Disconnecting...")
+    else:
+        await ctx.send("You have to be connected to the same voice channel to disconnect me.")
+
+
+# token
+bot.run('OTMyMDE1NjkxMTY1NDkxMjIx.YeM1QA.80vYLWaj7ulCWK8n8dJWLepkWkU')
